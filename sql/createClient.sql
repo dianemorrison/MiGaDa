305b065b645efed1621a62e7b24b5058a8c5f7c1
Gareth Fitzpatrick <gfitzp2002@yahoo.co.uk>
2023-10-12 14:45:27 +0100

    "Create a client table and add client- f_name and l_name - combined to clinet_name"

--- sql/createClient.sql
+++ sql/createClient.sql
@@ -1,15 +1,14 @@
+USE KainoosSoftworn_MiaG;
 -- 3. As a member of the Sales team I want to be able to create a new client. 
 -- I should be able to store a name, address and phone number
 
 CREATE TABLE client (
     client_id int PRIMARY KEY AUTO_INCREMENT,
-    f_name varchar(255),
-    l_name varchar(255),
+    client_name varchar(255),
     client_address varchar(255),
     phone_num varchar(20)
 );
 
-INSERT INTO client(f_name, l_name, client_address, phone_num)
-VALUES('John', 'Smith', '40 Botanic Avenue', '028 9087 0987'), 
-('Jane', 'Doe' '6 Jersey Avenue', '028 9087 1234'),
-('David', 'Jones',  '50 Donegal Road', '028 1234 0987');
+
+INSERT INTO client(client_name, client_address, phone_num)
+VALUES('Kainos', '40 Botanic Avenue', '028 9087 0987');
